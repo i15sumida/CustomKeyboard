1dedab25d58d1ede7cd38690728a364f094ac63c
住田みらの <i15sumida@tokuyama.kosen-ac.jp>
2019-10-25 11:33:06 +0900

    Create CustomKeyboardTests.swift

---
+++ CustomKeyboardTests/CustomKeyboardTests.swift
@@ -0,0 +1,34 @@
+//
+//  CustomKeyboardTests.swift
+//  CustomKeyboardTests
+//
+//  Created by 住田みらの on 2019/07/12.
+//  Copyright © 2019 住田みらの. All rights reserved.
+//
+
+import XCTest
+@testable import CustomKeyboard
+
+class CustomKeyboardTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
